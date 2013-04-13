e8206bdedaf174943d7c118b78c5c34baac46251
budblack <budblack@qq.com>
2013-04-13 14:18:43 +0800

    first
    This is a start

---
+++ Broken/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Broken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(197, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "提取权限";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 120);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0\r\n1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(197, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "探测网关";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 135);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cuit机房工具";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Broken/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Broken
+{
+    public partial class Form1 : Form
+    {
+        public class usrdata
+        {
+            public static string surnam = null;
+        }
+        
+        private string RunCmd(string command)
+        {
+            #region 调用cmd
+            return(allfuc.RunCmd(command));
+            #endregion
+        }
+
+        private bool ChUsr()
+        {
+            #region 检查用户权限
+            if (RunCmd("net localgroup administrators").IndexOf(System.Environment.UserName) >= 0)
+            {
+                logadd("已有管理员权限\n若需注销重登陆，密码为用户名");
+                button1.Enabled = false;
+                button2.Enabled = true;
+                return true;
+            }
+            else
+            {
+                logadd("当前用户无权限，需要提权操作！");
+                button1.Enabled = true;
+                return false;
+            }
+            #endregion
+        }
+
+        private bool logadd(string str)
+        {
+            #region 记录状态
+            label1.Text = label1.Text + "\n" + str;
+            return true;
+            #endregion
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = null;
+            #region 释放root程序
+            byte[] buf = Properties.Resources.root1;
+            System.IO.FileStream f = new System.IO.FileStream(Application.StartupPath + "\\root.exe", System.IO.FileMode.Create);
+            f.Write(buf, 0, buf.Length);
+            f.Close();
+            #endregion
+
+            #region 读取用户
+            logadd("读取用户...");
+            usrdata.surnam = System.Environment.UserName;
+            logadd("当前用户为："+usrdata.surnam);
+            ChUsr();//确定当前账户已有管理员权限
+            #endregion
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region 提权操作
+            logadd("尝试提权...");
+            RunCmd("root \"net localgroup administrators " + usrdata.surnam + " /add\"");
+            label1.Text = null;
+            ChUsr();//检查用户权限
+            #endregion
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string gateway=null,str=null;
+            string[] strs;
+            int i;
+
+            #region 探测网关
+            logadd("开始探测网关...");
+//            str=RunCmd("tracert -d -h 1 www.cuit.edu.cn");//探测网关;此方法有误
+//            strs = str.Split(new char[]{ '\n'});
+//            gateway = strs[4].Substring(32);
+            str = RunCmd("route print");
+            strs = str.Split(new char[] { '\n' });
+            for (i = 0; i < strs.Length; i++)
+            {
+                #region 第一方案
+                if (strs[i].IndexOf("10.220.1.0") > -1 || strs[i].IndexOf("255.255.255.0") > -1)//路由表特征
+                {
+                    gateway = strs[i].Substring(36, 17).Replace(" ","");
+                    break;
+                }
+                #endregion
+
+                gateway = "127.0.0.1";
+            }
+            #endregion
+            if (gateway.Split(new char[] { '.' }).Length == 4)
+            {
+                #region 设置网关
+                logadd("探测到网关：" + gateway);
+                if (ChUsr())
+                {
+                    logadd("正在设置网关...");
+                    RunCmd("netsh interface ip set address name=\"本地连接\" gateway= " + gateway + " gwmetric=0");
+                    RunCmd("explorer \"http://www.google.com\"");
+                    logadd("完成");
+                }
+                else
+                {
+                    logadd("无权限");
+                }
+                #endregion
+                button2.Enabled = false;
+//                Application.Exit();
+            }
+            else
+            {
+                logadd("识别网关失败！");
+                logadd(strs[4]);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RunCmd("del root.exe");
+        }       
+        
+    }
+}
+
